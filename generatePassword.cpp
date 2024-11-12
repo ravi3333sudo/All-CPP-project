3612b5470b353f18d15f4fe300c18220278dd040
ravi3333sudo <raviraushan1313@gmail.com>
2024-11-12 21:40:25 +0530

    Add files via upload

---
+++ generatePassword.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <string>
+using namespace std;
+
+string getPassword(int length)
+{
+    string Password = "";
+    string characters = "aquickfoxjumpsoverthelazydogAQUICKBROWNFOXJUMPSOVERTHELAZYDOG0123456789";
+    int charSize = characters.size();
+    srand(time(0));
+    int randomIndex;
+    for (int i = 0; i < length; i++)
+    {
+        randomIndex = rand() % charSize;
+        Password = Password + characters[randomIndex];
+    }
+    return Password;
+}
+
+int main()
+{
+    int length;
+    cout << "Enter the length of the Password :: ";
+    cin >> length;
+    string password = getPassword(length);
+    cout << "Generate Password :: " << password;
+
+    return 0;
+}
